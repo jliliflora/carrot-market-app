523cf9b01b7bc0006c11764c5d283c92c6248b1b
jliliflora <jliliflora@wonderpeople.co.kr>
2023-07-11 18:35:41 +0900

    prisma client

--- libs/client.ts
+++ libs/client.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+const client = new PrismaClient();
+
+client.user.create({
+  data: {
+    email: "hi",
+    name: "hi",
+  },
+});
