c44897da063ae186709244b5103c7146d586bd39
jliliflora <syon704@gmail.com>
2024-12-10 16:04:14 +0900

    채팅 보내기 api 추가

--- pages/api/streams/[id]/messages.ts
+++ pages/api/streams/[id]/messages.ts
@@ -4,7 +4,7 @@ import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@/pages/libs/server/withHandler";
 import { withApiSession } from "@/pages/libs/server/withSesstion";
-import client from "../../libs/server/client";
+import client from "../../../libs/server/client";
 
 async function handler(
   req: NextApiRequest,
@@ -13,18 +13,30 @@ async function handler(
   //여기서 콘솔 찍으면 백엔드 콘솔에서 확인가능함
   const {
     query: { id },
+    body,
+    session: { user },
   } = req;
-  const stream = await client.stream.findUnique({
-    where: {
-      id: +id!,
+  const message = await client.message.create({
+    data: {
+      message: body.message,
+      stream: {
+        connect: {
+          id: +id!,
+        },
+      },
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
     },
   });
-  res.json({ ok: true, stream });
+  res.json({ ok: true, message });
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET"],
+    methods: ["POST"],
     handler,
   })
 );
