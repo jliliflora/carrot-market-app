7b1b1951dc0cf9d4151c59d6fdacf470071e2fc3
jliliflora <syon704@gmail.com>
2025-01-03 19:35:13 +0900

    관심상품, 판매내역, 구매내역 목록이 비어 있을 때 안내 메시지 추가

--- pages/components/product-list.tsx
+++ pages/components/product-list.tsx
@@ -16,8 +16,54 @@ interface ProductListResponse {
 }
 
 export default function ProductList({ kind }: ProductListProps) {
-  const { data } = useSWR<ProductListResponse>(`/api/users/me/${kind}`);
-  return data ? (
+  const { data, isLoading, error } = useSWR<ProductListResponse>(
+    `/api/users/me/${kind}`
+  );
+  // console.log(data);
+
+  if (isLoading) {
+    return <div className="pt-10 px-2 text-center">Loading...</div>;
+  }
+  if (error) {
+    return <div>에러가 발생했습니다!</div>;
+  }
+
+  if (!data || data[kind]?.length === 0) {
+    switch (kind) {
+      case "favs":
+        return (
+          <div className="pt-10 px-2 text-center text-gray-600">
+            관심상품 목록이 비어있어요.
+            <br /> 마음에 드는 상품을 즐겨찾기 해보세요! 🧡
+          </div>
+        );
+
+      case "sales":
+        return (
+          <div className="pt-10 px-2 text-center text-gray-600">
+            판매상품 목록이 비어있어요.
+            <br /> 상품을 등록하고 판매를 시작해보세요! 🥕
+          </div>
+        );
+
+      case "purchases":
+        return (
+          <div className="pt-10 px-2 text-center text-gray-600">
+            구매내역 목록이 비어있어요.
+            <br /> 마음에 드는 상품을 구매해보세요! 🥕
+          </div>
+        );
+
+      default:
+        return (
+          <div className="pt-10 px-2 text-center text-gray-600">
+            정보가 없습니다.
+          </div>
+        );
+    }
+  }
+
+  return (
     <>
       {data[kind]?.map((record) => (
         <Item
@@ -26,8 +72,9 @@ export default function ProductList({ kind }: ProductListProps) {
           title={record.product.name}
           price={record.product.price}
           hearts={record.product._count.favs}
+          imageUrl="https://ossisconsult.com/includes/uploads/projects/default.jpg"
         />
       ))}
     </>
-  ) : null;
+  );
 }
